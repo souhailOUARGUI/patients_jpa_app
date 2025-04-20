3cbdf3ccb98b4232a134f0d47e7814768a313b09
souhailOUARGUI <ouargui550@gmail.com>
2025-04-20 15:24:01 +0100

    Initial commit

---
+++ src/main/java/ma/enset/patients_jpa_app/PatientsJpaAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.patients_jpa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsJpaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsJpaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/patients_jpa_app/PatientsJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.patients_jpa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsJpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
